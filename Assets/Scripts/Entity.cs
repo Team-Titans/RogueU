8a36cfda19cffc6fac890c6c6788c0b44379bd5e
TM <tyler.miller@students.aie.edu.au>
2015-05-19 15:40:18 -0700

    Movement prototype script, entity class, tile class, tilehandler class added.

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts
+{
+	class Entity
+	{
+		public int X;
+		public int Y;
+		public int ID;
+	}
+}
---
+++ Assets/Scripts/TileHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts
+{
+	class TileHandler
+	{
+		List<Tile> Tiles;
+
+		List<Tile> GetChanged()
+		{
+			List<Tile> tempList = new List<Tile>();
+
+			for (int i = 0; i < Tiles.Count(); i++)
+			{
+				if (Tiles[i].HasChanged)
+				{
+					Tiles[i].HasChanged = false;
+					tempList.Add(Tiles[i]);
+				}
+			}
+
+				return tempList;
+		}
+
+		//check all tiles if haschanged is true
+		//if true add x, y, id to list of entity to return;
+	}
+}
