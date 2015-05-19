183088196e5b7f9bae7d2cbecf8cfabef1a5e0ea
Aidan Nabass <ai.nabass@gmail.com>
2015-05-19 16:49:19 -0700

    Initial Player Class

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Assets.Scripts
+{
+	//Inherits from  Carbon
+	class Player : Carbon
+	{
+		public int XP;
+		public int Rank;
+		public int Gold;
+
+		//NOTE: Possible class
+		public List<int> Inventory;
+
+		public void Equip(Item a_Item);
+		public void Use(Item a_Item);
+		//Pickup from MoveTo of Carbon
+	}
+
+}
