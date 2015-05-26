5e1b82403a42659d26f52bd68dc048aeea26379e
Aidan Nabass <ai.nabass@gmail.com>
2015-05-26 14:22:10 -0700

    Gold Pickup + Add Score

---
+++ Assets/Scripts/PlayerScore.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScore : MonoBehaviour {
+
+	public int score = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		//Pickup Gold on Collision + Add Score
+		if (col.gameObject.tag == "Gold")
+		{
+			Debug.Log("He touched the gold!");
+			Destroy(col.gameObject);
+			score++;
+		}
+	}
+}
