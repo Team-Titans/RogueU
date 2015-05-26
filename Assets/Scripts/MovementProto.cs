442d3d8a60756b7242b8c2f0c5ae24bbdfbbe7aa
TM <tyler.miller@students.aie.edu.au>
2015-05-26 12:02:29 -0700

    Player moving around a "grid" with constraints, code to determine enemy turn

--- Assets/Scripts/MovementProto.cs
+++ Assets/Scripts/MovementProto.cs
@@ -6,43 +6,57 @@ public class MovementProto : MonoBehaviour
 {
 	private float TileH;
 	private float TileW;
-	
-	
 
-	private float GridX;
-	private float GridY;
+	public GameObject MoveBox;
+
+	public float Health;
+	public float GridX;
+	public float GridY;
+	public float GridXMax;
+	public float GridXMin;
+	public float GridYMax;
+	public float GridYMin;
 	// Use this for initialization
-	void Start () 
+	void Start ()
 	{
 		//Default values, change these to wherever this needs to spawn
-		GridX = 0;
-		GridY = 0;
+		GridX = 0.5f;
+		GridY = 0.5f;
+
+
+		//This sets the grid constraints and assumes the grid is perfectly square and the middle is zero
+		GridXMax = 7 + GridX;
+		GridYMax = 7 + GridY;
+		GridXMin = -GridXMax;
+		GridYMin = -GridYMax;
 
 		//This gets the width and height of the current item
 		TileW = transform.localScale.x;
 		TileH = transform.localScale.y;
 
-		//Moves this to wherever the
+		//Moves this to wherever the default is
 		transform.Translate((GridX * TileW) - transform.localPosition.x, (GridY * TileH) - transform.localPosition.y, 0);
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		if (Input.GetKeyDown(KeyCode.RightArrow))
+		
+		if (Input.GetKeyDown(KeyCode.RightArrow) && GridX < GridXMax)
 		{
 			GridX++;
+			
 		}
-		else if (Input.GetKeyDown(KeyCode.LeftArrow))
+		else if (Input.GetKeyDown(KeyCode.LeftArrow) && GridX > GridXMin)
 		{
 			GridX--;
 		}
-		else if (Input.GetKeyDown(KeyCode.UpArrow))
+		else if (Input.GetKeyDown(KeyCode.UpArrow) && GridY < GridYMax)
 		{
 			GridY++;
 		}
 
-		else if (Input.GetKeyDown(KeyCode.DownArrow))
+		else if (Input.GetKeyDown(KeyCode.DownArrow) && GridY > GridXMin)
 		{
 			GridY--;
 		}
