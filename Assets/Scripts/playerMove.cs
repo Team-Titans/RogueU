304db148d1b73718f9f23991d1386b0bcd8a9074
Wade Gushikuma <wade.gushikuma@students.aie.edu.au>
2015-05-26 11:51:14 -0700

    enemy seek

---
+++ Assets/Scripts/playerMove.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.GetKeyDown(KeyCode.A))
+        {
+            gameObject.transform.Translate(new Vector3(-gameObject.transform.localScale.x * 5f, 0, 0));
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            gameObject.transform.Translate(new Vector3(gameObject.transform.localScale.x * 5f, 0, 0));
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            gameObject.transform.Translate(new Vector3(0, gameObject.transform.localScale.x * 5f, 0));
+        }
+        else if(Input.GetKeyDown(KeyCode.S))
+        {
+            gameObject.transform.Translate(new Vector3(0, -gameObject.transform.localScale.x * 5f, 0));
+        }
+	}
+}
