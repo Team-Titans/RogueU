eeed54786bdda96a632a1e188ee0a807c6c900e7
Aidan Nabass <ai.nabass@gmail.com>
2015-06-01 11:12:58 -0700

    Enemies Spawn randomly like gold does.

--- Assets/Scripts/EnemyProto.cs
+++ Assets/Scripts/EnemyProto.cs
@@ -14,13 +14,32 @@ public class EnemyProto : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
 	{
+		//Default values, change these to wherever this needs to spawn
+		GridX = Random.Range(0, 15) - 7;
+		if (GridX < 0)
+		{
+			GridX -= .5f;
+		}
+		else
+		{
+			GridX += .5f;
+		}
+		GridY = Random.Range(0, 15) - 7;
+		if (GridY < 0)
+		{
+			GridY -= .5f;
+		}
+		else
+		{
+			GridY += .5f;
+		}
 
 		//This gets the width and height of the current item
 		TileW = transform.localScale.x;
 		TileH = transform.localScale.y;
 
-		GridX = -5.5f;
-		GridY = -5.5f;
+		//Set position
+		transform.position = new Vector3(GridX, GridY, -2);
 
 	}
 	bool IsBeside(float tX, float tY, float uX, float uY)
