1807a4379ba37c8b02fc70151fdabd88e1c3b5b0
TM <tyler.miller@students.aie.edu.au>
2015-06-01 17:55:50 -0700

    Removing DeleteMe.exe and dependencies

--- Assets/Scripts/getHighScores.cs
+++ Assets/Scripts/getHighScores.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
-using UnityEngine;
 
 public class getHighScores : MonoBehaviour {
     public struct PlayerScore
