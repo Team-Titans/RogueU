80495d6216f5b192ad17f1f7dedf056b9aa5073a
Aidan Nabass <ai.nabass@gmail.com>
2015-06-08 16:42:31 -0700

    HUD includes current Level

---
+++ Assets/hudLevel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class hudLevel : MonoBehaviour {
+
+	LevelLoad levelLoad;
+	Text text;
+	// Use this for initialization
+	void Start () {
+		levelLoad = FindObjectOfType<LevelLoad>();
+		text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = "L: " + levelLoad.currentLevel.ToString();
+	}
+}
