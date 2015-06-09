45599576f953eba3cb32994dbcf1821ca6cf1a37
Wade Gushikuma <wade.gushikuma@students.aie.edu.au>
2015-06-09 09:39:04 -0700

    lighting now working

---
+++ Assets/Scripts/lighting.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lighting : MonoBehaviour
+{
+
+    public GameObject CarbonToFollow;
+    public Light DirLight;
+    Transform trans;
+
+    // Use this for initialization
+    void Start()
+    {
+        trans = CarbonToFollow.transform.GetChild(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DirLight.transform.position = new Vector3(trans.position.x, trans.position.y, -15);
+        //Debug.Log(CarbonToFollow.transform.position.x + "," + CarbonToFollow.transform.position.y);
+    }
+}
